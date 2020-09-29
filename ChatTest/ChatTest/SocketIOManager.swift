fbeb9e3132328787c47f5c64098f3f7859233c80
jo030225 <whwngur123@naver.com>
2020-09-30 00:29:03 +0900

    .

---
+++ ChatTest/ChatTest/SocketIOManager.swift
@@ -0,0 +1,40 @@
+//
+//  SocketIOManager.swift
+//  ChatTest
+//
+//  Created by 조주혁 on 2020/09/30.
+//
+
+import UIKit
+import SocketIO
+
+class SocketIOManager: NSObject {
+    static let shared = SocketIOManager()
+    var manager = SocketManager(socketURL: URL(string: "http://localhost:9000")!, config: [.log(true), .compress])
+    var socket: SocketIOClient!
+    override init() {
+        super.init()
+        socket = self.manager.socket(forNamespace: "/test")
+        
+        socket.on("test") { dataArray, ack in
+            print(dataArray)
+        }
+    }
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func sendMessage(message: String, nickname: String) {
+        socket.emit("event", ["message" : "This is a test message"])
+        socket.emit("event1", [["name" : "ns"], ["email" : "@naver.com"]])
+        socket.emit("event2", ["name" : "ns", "email" : "@naver.com"])
+        socket.emit("msg", ["nick": nickname, "msg" : message])
+        
+    }
+
+}
