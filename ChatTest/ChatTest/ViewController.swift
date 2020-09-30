5b0f1987980c308d8d0b033227d851d894730ac9
jo030225 <whwngur123@naver.com>
2020-09-30 23:24:45 +0900

    소켓 구현

--- ChatTest/ChatTest/ViewController.swift
+++ ChatTest/ChatTest/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import SocketIO
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func disconnetBtn(_ sender: Any) {
+        SocketIOManager.shared.closeConnection()
+    }
+    
+    @IBAction func connetBtn(_ sender: Any) {
+        SocketIOManager.shared.establishConnection()
+    }
+    
+    @IBAction func sendData(_ sender: Any) {
+        SocketIOManager.shared.sendMessage(message: self.textField.text!, nickname: "ns")
+    }
 }
 
